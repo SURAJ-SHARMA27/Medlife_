f3c3caac0855e8564a28d633d88003b31c8639c8
AKSHAT SINGH <81671650+Akshatsingh18@users.noreply.github.com>
2022-05-28 21:20:04 +0530

    Add files via upload

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,18 +1,13 @@
-const async = require('hbs/lib/async')
-const mongoose = require('mongoose')
 
-const connectDb = async() =>{
-    try{
-        const conn = await mongoose.connect(process.env.MONGO_URI,{
-            useUnifiedTopology:true,
-            useNewUrlParser:true,
-        });
-        console.log('Database connected  ' + conn.connection.host);
-    }
-    catch (error){
-        console.log('Error: ' + error.message );
-        process.exit(1);
-    }
-}; 
+const mysql = require('mysql2');
 
-module.exports = connectDb;
+const connectDb = mysql.createPool({
+
+    host:'localhost',
+    user:'root',
+    database:'medlife',
+    password:'medlife'
+});
+
+
+module.exports = connectDb.promise();
--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,65 @@ const products = require("./data/product")
 const dotenv = require('dotenv')
 const connectDb = require('./config/config')
 const product = require('./data/product')
+const bodyParser=require("body-parser")
+var cors = require('cors');
 
+const encoder = bodyParser.urlencoded();
 //dotenv config
 dotenv.config();
-//connecting to mongodb database
-connectDb();
+//connecting to mysql database
+connectDb.execute('SELECT * FROM logindb')
+  .then(result => {
+    console.log(result[0]);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-
-
-app.get("/",(req,res) => 
+app.post("/signup",encoder, (req,res) => 
 {
-    res.send("<h1>Welcome to medlife</h1>");
+      const emailid = req.body.email;
+      const password = req.body.password;
+      const Firstname = req.body.Fname;
+      const Lastname = req.body.Lname;
+      
+      connectDb.query("INSERT INTO logindb (emailid ,password, firstname,lastname) VALUES (?,?,?,?)",
+      [emailid,password,Firstname,Lastname],(err, result,field)=>{
+
+        console.log(err);
+        res.redirect("/login");
+      }
+      );
+});
+
+app.post("/login",encoder, (req,res) => 
+{
+      const emailid = req.body.email;
+      const password = req.body.password;
+      
+      connectDb.query("SELECT * From logindb WHERE emailid = ? AND password = ?",
+      [emailid,password],(err, result)=>{
+        if(err)
+        {
+          res.send({err:err})
+        }
+
+        if(result.length >0)
+        {
+          res.send(result);
+        }
+        else
+        {
+          res.send("message:wrong username password");
+        }
+        
+      });
+    
 });
 
 app.get("/product",(req,res)=>
@@ -30,8 +76,11 @@ app.get("/product/:ids" ,(req,res) => {
 
 });
 const PORT = 8080;
-app.listen(process.env.PORT||PORT,()=>{
-    console.log('server is running in '+ process.env.NODE_ENV + ' mode on port ' +  PORT)  ;
+// app.listen(process.env.PORT||PORT,()=>{
+//     console.log('server is running in '+ process.env.NODE_ENV + ' mode on port ' +  PORT)  ;
+// });
+app.listen(8080,()=>{
+  console.log("its working")
 });
 
 
